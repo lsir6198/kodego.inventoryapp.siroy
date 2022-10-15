fd78479d0912f57f3bb3a604d72000da7e8a2495
lesa siroy <lesa@kodego.ph>
2022-10-15 17:22:44 +0800

    test activity

--- app/src/main/java/com/kodego/inventory/app/siroy/TestActivity.kt
+++ app/src/main/java/com/kodego/inventory/app/siroy/TestActivity.kt
@@ -2,6 +2,8 @@ package com.kodego.inventory.app.siroy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.RadioGroup
 import android.widget.Toast
 import com.kodego.inventory.app.siroy.databinding.ActivityTestBinding
 
@@ -20,5 +22,37 @@ class TestActivity : AppCompatActivity() {
             binding.txtName.text = name
 
         }
+
+        binding.rgRadioGroup.setOnCheckedChangeListener{rgRadioGroup, checkOption ->
+            when(checkOption){
+                R.id.rgButton1 -> Toast.makeText(applicationContext, "Option 1 Selected", Toast.LENGTH_LONG).show()
+                R.id.rgButton2 -> Toast.makeText(applicationContext, "Option 1 Selected", Toast.LENGTH_LONG).show()
+                R.id.rgButton3 -> Toast.makeText(applicationContext, "Option 1 Selected", Toast.LENGTH_LONG).show()
+            }
+
+
+        }
+
+         binding.cb1.setOnClickListener(){
+             if (binding.cb1.isChecked){
+                 Toast.makeText(applicationContext, "CheckBox 1 Checked", Toast.LENGTH_LONG).show()
+
+             }else{
+                 Toast.makeText(applicationContext,"CheckBox 1 Unchecked", Toast.LENGTH_LONG).show()
+             }
+
+             if (binding.cb2.isChecked){
+                 Toast.makeText(applicationContext, "CheckBox 2 Checked", Toast.LENGTH_LONG).show()
+
+             }else{
+                 Toast.makeText(applicationContext,"CheckBox 2 Unchecked", Toast.LENGTH_LONG).show()
+             }
+
+         }
+
+         val data = arrayListOf<String>("Option 1", "Option 2", "Option 3")
+         val adapterParent = ArrayAdapter(applicationContext, R.layout.textview,data)
+
+         binding.spSpin.adapter = adapterParent
     }
 }
