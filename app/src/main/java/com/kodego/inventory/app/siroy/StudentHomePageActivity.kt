bc24df65ee39f233dd2f9d6de914285350f908ba
lesa siroy <lesa@kodego.ph>
2022-10-16 18:20:07 +0800

    StudentHomePage

---
+++ app/src/main/java/com/kodego/inventory/app/siroy/StudentHomePageActivity.kt
@@ -0,0 +1,11 @@
+package com.kodego.inventory.app.siroy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentHomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_home_page)
+    }
+}
